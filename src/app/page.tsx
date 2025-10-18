c8b05db265d848f8e17d272bdb66c9b32d79abc6
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 <sabaudianin@gmail.com>
2025-10-18 18:57:50 +0200

    feat hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
-import { Nav } from "@/components/Nav/Nav";
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
-  return <Nav />;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 items-center px-2 py-8 lg:py-16">
+      <div className=" font-display py-32">
+        <h1 className="text-[clamp(2.5rem,5vw,4rem)] mb-2 font-bold">
+          Crafted with Love and passion
+        </h1>
+        <p className="text-[var(--medium)] ">
+          Locally fresh ingredients, wood fire , italian receipe create
+          unforgettable memories
+        </p>
+        <div className="text-center">
+          <button className="mt-8 rounded-xl bg-[var(--success)] py-4 px-6 font-semibold text-white hover:text-black transition-colors duration-300 hover:translate-y-[-2px] transition duration-200 shadow-md">
+            Show Menu
+          </button>
+        </div>
+      </div>
+      <div className="hidden lg:block object-cover">
+        <Image
+          src="/pizza.png"
+          alt="Pizza-hero Picture"
+          width={800}
+          height={600}
+          className="transform transition-transform duration-200 hover:scale-105 "
+        />
+      </div>
+    </section>
+  );
 }
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,45 +3,43 @@ import Link from "next/link";
 
 export const Nav = () => {
   return (
-    <div className=" fixed top-0 w-full z-50 backdrop-blur-[10px] px-6 py-6 lg:py-12 bg-[var(--navColor)]  ">
-      <section className="mx-auto flex justify-between items-center  max-w-7xl">
-        <p className="text-xl lg:text-4xl font-bold text-[var(--success)]">
-          Pizza
-          <span className="text-white [-webkit-text-stroke:1px_rgba(0,0,0,0.75)] [text-stroke:1px_rgba(0,0,0,0.75)] drop-shadow-[0_1px_1px_rgba(0,0,0,0.5)]">
-            4
-          </span>
-          <span className="text-[var(--error)]">Real</span>
-        </p>
-        <nav className="w-1/2 flex justify-around lg:text-2xl gap-1">
-          <Link
-            href="#menu"
-            className="hover:text-[var(--error)] transition-colors durtion-300 font-medium"
-          >
-            Menu
-          </Link>
-          <Link
-            href="#features"
-            className="hover:text-[var(--error)] transition-colors duration-300 font-medium"
-          >
-            Features
-          </Link>
-          <Link
-            href="#contact"
-            className="hover:text-[var(--error)] transition-colors duration-300 font-medium"
-          >
-            Contact
-          </Link>
-          <Link
-            href="#about"
-            className="hover:text-[var(--error)] transition-colors duration-300 font-medium"
-          >
-            About
-          </Link>
-        </nav>
-        <button className="bg-[var(--success)] px-4 py-2 cursor-pointer rounded-xl transition-transform duration-300  hover:translate-y-[-2px] hover:shadow-lg font-bold lg:text-xl">
-          Order Now
-        </button>
-      </section>
-    </div>
+    <section className="mx-auto flex justify-between items-center max-w-screen-2xl">
+      <p className="text-xl lg:text-4xl font-bold text-[var(--success)]">
+        Pizza
+        <span className="text-white [-webkit-text-stroke:1px_rgba(0,0,0,0.75)] [text-stroke:1px_rgba(0,0,0,0.75)] drop-shadow-[0_1px_1px_rgba(0,0,0,0.5)]">
+          4
+        </span>
+        <span className="text-[var(--error)]">Real</span>
+      </p>
+      <nav className="w-1/2  justify-around lg:text-2xl gap-1 hidden lg:flex">
+        <Link
+          href="#menu"
+          className="hover:text-[var(--error)] transition-colors duration-300 font-medium"
+        >
+          Menu
+        </Link>
+        <Link
+          href="#features"
+          className="hover:text-[var(--error)] transition-colors duration-300 font-medium"
+        >
+          Features
+        </Link>
+        <Link
+          href="#contact"
+          className="hover:text-[var(--error)] transition-colors duration-300 font-medium"
+        >
+          Contact
+        </Link>
+        <Link
+          href="#about"
+          className="hover:text-[var(--error)] transition-colors duration-300 font-medium"
+        >
+          About
+        </Link>
+      </nav>
+      <button className="bg-[var(--error)] px-4 py-2 cursor-pointer rounded-2xl transition-transform duration-300  hover:translate-y-[-2px] hover:shadow-lg font-bold lg:text-xl text-white">
+        Order Now
+      </button>
+    </section>
   );
 };
